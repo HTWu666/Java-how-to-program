ba2acf19b915d97609c89d73faa7319b21c89a14
HTWu666 <126232123+HTWu666@users.noreply.github.com>
2023-05-12 16:52:16 +0800

    Update Prime.java

--- src/ch06/ex6_25/Prime.java
+++ src/ch06/ex6_25/Prime.java
@@ -1,5 +1,16 @@
 //application to check a number is prime or not.
 
+/*6.25 (Prime Numbers)A positive integer is prime if it’s divisible by only 1
+and itself. For example, 2, 3, 5 and 7 are prime, but 4, 6, 8 and 9 are not.
+The number 1, by definition, is not prime
+1. Write a application and method that determines whether a number is prime.
+2. Use this method in an application that determines and displays all the prime
+numbers less than 10,000. How many numbers up to 10,000 do you have to test to
+ensure that you’ve found all the primes?
+3. Initially, you might think that n/2 is the upper limit for which you must test
+to see whether a number n is prime, but you need only go as high as the square root
+of n. Rewrite the program, and run it both ways.*/
+
 import java.util.Scanner;
 
 public class Prime_ {
