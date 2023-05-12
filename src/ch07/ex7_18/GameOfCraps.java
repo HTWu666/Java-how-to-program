dfa6632594d80ce751dbf7b522ab027e4664446a
HTWu666 <126232123+HTWu666@users.noreply.github.com>
2023-05-12 16:53:48 +0800

    Update GameOfCraps.java

--- src/ch07/ex7_18/GameOfCraps.java
+++ src/ch07/ex7_18/GameOfCraps.java
@@ -1,3 +1,11 @@
+
+/*7.18 (Game of Craps) Write an application that runs 1,000,000 games of craps (Fig. 6.8 ) and answers the following questions:
+1. How many games are won on the first roll, second roll, …,twentieth roll and after the twentieth roll?
+2. How many games are lost on the first roll, second roll, …,twentieth roll and after the twentieth roll?
+3. What are the chances of winning at craps? [Note: You shoulddiscover that craps is one of the fairest casino games. What doyou suppose this means?]
+4. What is the average length of a game of craps?
+5. Do the chances of winning improve with the length of the game?*/
+
 package ex7_18;
 
 import java.security.SecureRandom;
