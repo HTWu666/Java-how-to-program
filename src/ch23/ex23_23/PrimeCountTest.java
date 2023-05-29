1d4963f3147a6f70d8e3d10931e61e0263277d71
HTWu666 <126232123+HTWu666@users.noreply.github.com>
2023-05-29 14:42:19 +0800

    Create PrimeCountTest.java

---
+++ src/ch23/ex23_23/PrimeCountTest.java
@@ -0,0 +1,18 @@
+package ex23_23;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.ExecutorService;
+
+public class primeCountTest {
+	public static void main(String[] args) {
+		PrimeCount primeCounter1 = new PrimeCount(2, 10000);
+		PrimeCount primeCounter2 = new PrimeCount(10000, 20000);
+		
+		ExecutorService executorService = Executors.newCachedThreadPool();
+		executorService.execute(primeCounter1);
+		executorService.execute(primeCounter2);
+		
+		executorService.shutdown();
+	}
+
+}
