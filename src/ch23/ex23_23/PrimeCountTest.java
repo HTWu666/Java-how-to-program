687c0a6b0d908b3c2cf2720e2c8fdec389eb17a1
HTWu666 <126232123+HTWu666@users.noreply.github.com>
2023-05-29 14:42:34 +0800

    Update PrimeCountTest.java

--- src/ch23/ex23_23/PrimeCountTest.java
+++ src/ch23/ex23_23/PrimeCountTest.java
@@ -3,7 +3,7 @@ package ex23_23;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ExecutorService;
 
-public class primeCountTest {
+public class PrimeCountTest {
 	public static void main(String[] args) {
 		PrimeCount primeCounter1 = new PrimeCount(2, 10000);
 		PrimeCount primeCounter2 = new PrimeCount(10000, 20000);
